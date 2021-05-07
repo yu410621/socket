a8434fbdaab9df7bea2a39e2cc66368a49198750
yu410621 <1275709810@qq.com>
2021-05-07 15:31:06 +0800

    计算机服务程序

--- hello_client.c
+++ hello_client.c
@@ -4,9 +4,48 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+
+#define BUF_SIZE 1024
 void error_handling(char *message);
 
 int main(int argc, char *argv[]){
+
+    int sock;
+    int str_len, recv_len, recv_cnt;
+    struct sockaddr_in serv_addr;
+    char message[BUF_SIZE];
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error");
+    else
+        printf("Connected.......");
+
+    while(1){
+        fputs("Input message(Q to quit): ", stdout);
+        fgets(message, BUF_SIZE, stdin);
+        if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+            break;
+        str_len = write(sock, message, strlen(message));
+
+        recv_len = 0;
+        while(recv_len < str_len){
+            recv_cnt = read(sock, &message, BUF_SIZE - 1);
+            if(recv_cnt == -1)
+                error_handling("read() error");
+            recv_len += recv_cnt; 
+        }
+        message[recv_len] = 0;
+        printf("Message from server: %s\n", message);
+    }
+    close(sock);
 
     return 0;
 }
--- hello_server.c
+++ hello_server.c
@@ -7,6 +7,43 @@
 void error_handling(char *message);
 
 int main(int argc, char *argv[]){
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr_size);
+
+    char meassage[1024] = "hello";
+
+    if(argc != 2){
+        printf("Usage : argv[0] <port> ");
+        exit(1);
+    }
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
+        error_handling("socket() error");
+    
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+    
+    if(listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+    
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    if(clnt_sock == -1)
+        error_handling("accept() error");
+    
+    write(clnt_sock, meassage, sizeof(meassage));
+    while(read(clnt_sock, meassage, 1024)!= -1){
+        write(clnt_sock, meassage, strlen(meassage));
+    }
+    close(clnt_sock);
+    close(serv_sock);
+
 
     return 0;
 }
